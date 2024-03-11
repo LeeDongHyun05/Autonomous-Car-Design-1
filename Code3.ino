e1b37880f0a2863bbb7172e8d3a0ba8380cac160
LeeDongHyun05 <159413137+LeeDongHyun05@users.noreply.github.com>
2024-03-11 12:31:02 +0900

    Add files via upload

---
+++ Code3.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("test!");
+  Serial.println("test!");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
